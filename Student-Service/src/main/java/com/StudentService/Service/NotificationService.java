8d529d60f9bc403dda10277b7c72ee179750d467
ronakyadav21 <ronakyadav9977@gmail.com>
2026-06-01 10:36:17 +0530

    changes are made

--- Student-Service/src/main/java/com/StudentService/Service/NotificationService.java
+++ Student-Service/src/main/java/com/StudentService/Service/NotificationService.java
@@ -1,9 +1,7 @@
 package com.StudentService.Service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.StudentService.Model.Notification;
 import com.StudentService.Repository.NotificationRepository;
 
